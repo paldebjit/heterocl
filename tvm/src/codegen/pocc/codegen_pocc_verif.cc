eb3d6fdb461de3884fcdb28b121f016f7c26aa0e
Debjit Pal <work.debjitpal@gmail.com>
2021-04-08 13:47:14 -0400

    Initiating verif integration between PoCC and HeteroCL

---
+++ tvm/src/codegen/pocc/codegen_pocc_verif.cc
@@ -0,0 +1,19 @@
+/*!
+ *  Copyright (c) 2021 by Contributors
+ * \file codegen_pocc_util.cc
+ */
+#include <tvm/runtime/config.h>
+#include <tvm/packed_func_ext.h>
+#include <vector>
+#include <string>
+#include <tuple>
+#include <regex>
+#include "./codegen_pocc.h"
+#include "../../runtime/thread_storage_scope.h"
+
+namespace TVM {
+namespace codegen {
+
+
+}  // namespace codegen
+}  // namespace TVM
